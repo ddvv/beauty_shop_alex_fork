568bc4308c4b0247cb94638ce719a877c10ae399
Aleksandr Alekhin <alekhin.aleks92@gmail.com>
2024-11-09 19:02:05 +0100

    added header and etc

---
+++ src/components/Banner.jsx
@@ -0,0 +1,22 @@
+import bannerImg from '../assets/img/banner.svg'
+
+function Banner () {
+    return (
+    <>
+    <section className="first-screen">
+      <div className="rectangle-2"></div>
+      <div
+        className="rectangle-3"
+        style={{ backgroundImage: `url(${bannerImg})` }}
+      ></div>
+      <div className="banner-content">
+        <h1>DISCOVER YOUR INNER BEAUTY<br />WITH BLOSSOM GLOW KIT</h1>
+        <p>Great gift for yourself and loved ones</p>
+        <button className="banner-button" onClick={()=>{alert('Working')}}>Shop Now</button>
+      </div>
+    </section>
+    </>
+  );
+}
+
+export default Banner;
---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import search from '../assets/img/search.svg';
+import account from '../assets/img/account.svg';
+import cart from '../assets/img/cart.svg'
+
+function Header () {
+    return (
+    <header className="header">
+      <div className="header-container">
+        <h3 className="title">Bloom <span>Beauty</span></h3>
+         <nav className="header-nav">
+            <a href="/">SHOP ALL</a>
+            <a href="/">BESTSELLERS</a>
+            <a href="/">COLLECTION</a>
+            <a href="/">ABOUT US</a>
+            <a href="/">BLOG</a>
+        </nav>
+        <div className="icons">
+            <div className="icon-item">
+            <img src={search} alt='search'/>
+            <span>SEARCH</span>
+            </div>
+            <div className="icon-item">
+            <img src={account} alt='account'/>
+            <span>ACCOUNT</span>
+            </div>
+            <div className="icon-item">
+            <img src={cart} alt='cart'/>
+            <span>CART</span>
+            </div>
+        </div>
+        </div>
+    </header>
+    )
+}
+
+export default Header;
