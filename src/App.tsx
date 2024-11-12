6d2a3ab51a2d972ce215398c064564b5976bed90
Aleksandr Alekhin <alekhin.aleks92@gmail.com>
2024-11-12 09:37:59 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import BlossomIMG1 from './assets/img/IMAGE1.svg';
 import BlossomIMG2 from './assets/img/IMAGE.svg';
 import Blog from "./components/Blog"
 import Quiz from "./components/SkinQuiz"
-import InstSection from "./components/InstSection"
+// import InstSection from "./components/InstSection"
 
 
 function App() {
