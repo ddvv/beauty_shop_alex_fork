42b57c01ae75eb89399fbf935635a37cdc4cb4a9
Aleksandr Alekhin <alekhin.aleks92@gmail.com>
2024-11-12 16:11:07 +0100

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    filename: '[name].[contenthash].js',
+    path: resolve(__dirname, 'build'),
+    publicPath: '/',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 3001,
+    open: true,
+    hot: true,
+    historyApiFallback: {
+      disableDotRule: true,
+    },
+  },
+};
