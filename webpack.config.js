a801d7e39f408ccbcc34cb983d02ad4a981313c2
Danilov Dmitriy <dima-danilov1995@gmail.com>
2024-11-14 17:41:24 +0300

    workflow

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: resolve(__dirname, 'public', 'index.html'),
     }),
   ],
   devtool: 'inline-source-map',
