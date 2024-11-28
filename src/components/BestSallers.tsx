55db1e63bbfd7e6297aa8e903dbde1b0a2c8cf04
Aleksandr Alekhin <alekhin.aleks92@gmail.com>
2024-11-28 18:43:41 +0100

    css

--- src/components/BestSallers.tsx
+++ src/components/BestSallers.tsx
@@ -1,10 +1,16 @@
 import Item from "../components/Item/Item"
+import flowerLeft from "../../public/assets/img/leftFlower.png"
+import flowerRight from "../../public/assets/img/rightFlower.png"
 import '../styles/bestSallers.css'
 
 function BestSallers () {
     return (
         <>
-        <h2>BEST SALLERS</h2>
+        <div className="title-new-sellers">
+            <img src={flowerRight} alt='flower'/>
+                <h2>NEW ARRIVALS</h2>
+            <img src={flowerLeft} alt='flower'/>   
+        </div>
         <a href="/BEST SALLERS" className="see-all-sallers">See All</a>
         <div className="container-sallers">
             <Item/>
--- src/components/NewArrivals.tsx
+++ src/components/NewArrivals.tsx
@@ -1,10 +1,17 @@
 import Item from "../components/Item/Item"
+import flowerLeft from "../../public/assets/img/leftFlower.png"
+import flowerRight from "../../public/assets/img/rightFlower.png"
+
 import '../styles/newArrivals.css'
 
 function NewArrivals () {
     return (
         <>
-        <h2>NEW ARRIVALS</h2>
+        <div className="title-new-arrivals">
+            <img src={flowerRight} alt='flower'/>
+                <h2>NEW ARRIVALS</h2>
+            <img src={flowerLeft} alt='flower'/>   
+        </div>
         <a href="/NEW ARRIVALS" className="see-all-arrivals">See All</a>
         <div className="container-arrivals">
             <Item/>
